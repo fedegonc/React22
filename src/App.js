13990dad14ca1fca15e91f38a60a3516e19bdd0c
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-11-21 14:35:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,23 +4,15 @@ import Logo from './components/Logo';
 import Navbar from './components/Navbar';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar />  // Renderiza la barra de navegación en la parte superior del encabezado
-        <Logo />    // Muestra el logo de "Tu Leyenda" justo debajo del Navbar
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
+        <Logo />
+        
+        
       </header>
     </div>
   );
