3a843dfc5fffd7354b988cbc8b0997fd34b1075a
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-11-21 14:42:45 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@
 import React from 'react';
 import Logo from './components/Logo';
 import Navbar from './components/Navbar';
-import './App.css';
-
+// import './App.css'; // Eliminado para usar Bootstrap
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container text-center">
+      <header className="bg-light p-5 rounded">
         <Navbar />
         <Logo />
         
