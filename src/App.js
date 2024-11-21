31cd60c91f46fafba35b6b1395b05ff52abd9ce9
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-11-21 14:44:58 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import Logo from './components/Logo';
 import Navbar from './components/Navbar';
-// import './App.css'; // Eliminado para usar Bootstrap
 
 function App() {
   return (
