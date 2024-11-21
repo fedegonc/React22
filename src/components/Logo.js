c1ce628bf5f1ea1187dc28fdacdf88a25ee93358
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-11-21 14:43:32 -0300

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,9 @@
 // src/components/Logo.js
 import React from 'react';
-import './Logo.css';
+// import './Logo.css'; // Eliminado para usar Bootstrap
 
 function Logo() {
   return (
-    <h1 className="Logo">Tu Leyenda</h1>
+    <h1 className="display-4">Tu Leyenda</h1>
   );
 }
-
-export default Logo;
