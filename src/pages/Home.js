ba507a81aa3379821ed873924c89dc508a3382cd
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-11-22 21:37:14 -0300

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+// src/pages/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenido a la página principal</h1>
+    </div>
+  );
+};
+
+export default Home;
